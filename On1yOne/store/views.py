7df53195696daa37c4a2ff2067f6c40a1228a0f3
Satvik Batra <118475547+satvikbatra@users.noreply.github.com>
2024-02-26 12:44:47 +0530

    basic root

---
+++ On1yOne/store/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    context = {}
+    return render(request, 'store/home.html', context)
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
